2e4fe4c2531111692f98a6acb08bdcc1966fc64e
Idris Noor <idreesnoor150@gmail.com>
2023-11-08 16:09:21 +0300

    nex practice

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddToCart from './AddToCart';
+// import styles from './productCard.module.css';
+
+
+
+const ProductCard = () => {
+  return (
+    <div className='p-'>
+        <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
